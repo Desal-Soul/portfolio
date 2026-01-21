eae52111a731cb071808f3f07b1140149a5757c0
deschanela <alexandre.deschanel.666@gmail.com>
2026-01-21 14:36:10 +0100

    test temporaire avec https

--- src/applications/app-locales/opera/app/pages/page/page_reseau.js
+++ src/applications/app-locales/opera/app/pages/page/page_reseau.js
@@ -2,6 +2,6 @@ import {Page} from "../page.js";
 
 let contenue = document.createElement("div");
 contenue.innerHTML =
-    "<iframe src='https://www.linkedin.com/in/alexandre-deschanel/'>" +
+    "<iframe credentialless='true' src='ressources/documents/cv.pdf'>" +
     "</iframe>"
 export let page_reseau = new Page("reseau", contenue, [])
