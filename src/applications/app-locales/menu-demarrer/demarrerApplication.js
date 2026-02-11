621e80d03750b8d0fa1faa9a5694b7b0623f7018
deschanela <alexandre.deschanel.666@gmail.com>
2026-02-11 20:06:39 +0100

    gestion de la dissimulation du menu widows quand on click sur une iframe

--- src/applications/app-locales/menu-demarrer/demarrerApplication.js
+++ src/applications/app-locales/menu-demarrer/demarrerApplication.js
@@ -9,6 +9,29 @@ class demarrerApplication extends AbstractApplication {
         this._logo.addEventListener("click", (e) => {
             this.toggle();
         });
+        /**window.focus();
+
+        window.addEventListener("blur", (event) => {
+            setTimeout(() => {
+                if(document.activeElement.tagName === "IFRAME") {
+                    if (!document.getElementById('app_demarrer').classList.contains("hidden")){
+                        const window_logo = document.getElementById('bMenuWindows');
+                        const menu = document.getElementById('app_demarrer');
+                        if (!menu.contains(event.target) && !window_logo.contains(event.target)) {
+                            demarrer.toggle()
+                        }
+                    }
+                    console.log("Clic détecté dans une iframe");
+                }
+            }, 0);
+        });*/
+        var iframewatcher = setInterval(function(){
+            var activeE = document.activeElement;
+            if(activeE && activeE.tagName == 'IFRAME'){
+                if (!document.getElementById('app_demarrer').classList.contains("hidden")){ demarrer.toggle() }
+                window.focus();
+        } }, 100);
+
         document.addEventListener('click', function(event) {
             if (!document.getElementById('app_demarrer').classList.contains("hidden")){
                 const window_logo = document.getElementById('bMenuWindows');
