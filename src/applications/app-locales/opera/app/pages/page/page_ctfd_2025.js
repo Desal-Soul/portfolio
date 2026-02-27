ffb8bb877cf434ff60e18fe5ab99c54d135f5345
deschanela <alexandre.deschanel.666@gmail.com>
2026-02-27 20:57:46 +0100

    implementation de la page ctfd2025 et ajout d'une démo du site

--- src/applications/app-locales/opera/app/pages/page/page_ctfd_2025.js
+++ src/applications/app-locales/opera/app/pages/page/page_ctfd_2025.js
@@ -1,9 +1,7 @@
 import {ctfd} from "./tags/liste_de_tag.js";
 
 let contenue = document.createElement("div");
-contenue.innerHTML =
-    "<iframe src='ressources/documents/work_in_progress.html'>" +
-    "</iframe>"
+contenue.innerHTML = "<iframe src='ressources/documents/ctf2025.html'></iframe>";
 
 
 export let page_ctfd_2025 = ["Last Hour", contenue, ctfd.concat("2025/2026")]
---
+++ src/classes/Screensaver.js
@@ -0,0 +1,127 @@
+class Screensaver {
+    constructor({ secondsInactive, speed, logo, disabledWhenUsingIframes }) {
+        this.secondsInactive = secondsInactive
+        this.speed = speed
+        this.logo = logo
+        this.disabledWhenUsingIframes = disabledWhenUsingIframes
+        this.container = null
+        this.imgX = 0
+        this.imgY = 0
+        this.imgWidth = 0
+        this.imgHeight = 0
+        this.idleCounter = 0
+        this.movingDown = true
+        this.movingRight = true
+
+        this.enabled = false
+
+        const _this = this
+        const tmpImg = document.createElement("img")
+        tmpImg.src = logo
+        tmpImg.onload = function () {
+            _this.imgWidth = tmpImg.width * 0.5
+            _this.imgHeight = tmpImg.height * 0.5
+            _this.init()
+        }
+    }
+
+    init() {
+        this.container = document.createElement("div")
+        this.container.style.position = "fixed"
+        this.container.style.left = 0
+        this.container.style.right = 0
+        this.container.style.top = 0
+        this.container.style.bottom = 0
+        this.container.style.zIndex = 9999
+        this.container.style.cursor = "none"
+        this.container.style.display = "none"
+        this.container.style.outline = "none"
+        this.container.style.backgroundColor = "black"
+        this.container.style.userSelect = "none"
+        this.container.style.backgroundImage = `url(${this.logo})`
+        this.container.style.backgroundRepeat = "no-repeat"
+        this.container.style.backgroundSize = `${this.imgWidth}px ${this.imgHeight}px`
+
+        document.getElementsByTagName("BODY")[0].appendChild(this.container)
+
+        const _this = this
+        setInterval(_this.updateCounter.bind(_this), 1000)
+    }
+
+    updateCounter() {
+        const hasIframe = document.getElementsByTagName("iframe")[0] ? true : false
+        if (this.disabledWhenUsingIframes && hasIframe) { return }
+        this.idleCounter = this.idleCounter + 1
+        if (this.idleCounter >= this.secondsInactive) { if (!this.enabled) { this.startScreensaver() } }
+    }
+
+    startScreensaver() {
+        this.enabled = true
+
+        this.imgX = 0
+        this.imgY = 0
+
+        document.getElementsByTagName("BODY")[0].style.overflowY = "hidden"
+
+        this.container.style.display = "block"
+
+        window.scrollTo(0, 0)
+
+        this.animate()
+    }
+
+    stop() {
+        this.enabled = false
+
+        this.idleCounter = 0
+
+        document.getElementsByTagName("BODY")[0].style.overflowY = "initial"
+
+        this.container.style.display = "none"
+    }
+
+    animate() {
+        const _this = this
+        function loop() {
+            if (_this.enabled) {
+                _this.imgX = _this.movingRight ? _this.imgX + _this.speed : _this.imgX - _this.speed
+                _this.imgY = _this.movingDown ? _this.imgY + _this.speed : _this.imgY - _this.speed
+
+                _this.container.style.backgroundPositionX = _this.imgX.toFixed(0) + "px"
+                _this.container.style.backgroundPositionY = _this.imgY.toFixed(0) + "px"
+
+                if (_this.imgY > _this.container.offsetHeight - _this.imgHeight) { _this.movingDown = false }
+
+                if (_this.imgY < 0) { _this.movingDown = true }
+
+                if (_this.imgX > _this.container.offsetWidth) {
+                    _this.imgX = -_this.imgWidth
+                    _this.imgY = parseInt( Math.random() * _this.container.offsetHeight - _this.imgHeight )
+                    _this.movingDown = true
+                }
+
+                if (_this.imgX > _this.container.offsetWidth - _this.imgWidth) { _this.movingRight = false }
+
+                if (_this.imgX < 0) { _this.movingRight = true }
+
+                if (_this.imgY > _this.container.offsetHeight) {
+                    _this.imgY = -_this.imgHeight
+                    _this.imgX = parseInt( Math.random() * _this.container.offsetWidth - _this.imgWidth )
+                    _this.movingRight = true
+                }
+
+                setTimeout(loop, 20)
+            }
+        }
+
+        loop()
+    }
+}
+/**
+const myScreensaver = new Screensaver({
+    secondsInactive: 0,
+    speed: 2,
+    logo: '../picto/loading.png',
+    disabledWhenUsingIframes: true,
+});
+**/
