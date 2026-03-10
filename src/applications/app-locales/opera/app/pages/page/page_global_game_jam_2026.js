cb9b2587137362d4bf9070366a92d9f62ed9892f
deschanela <alexandre.deschanel.666@gmail.com>
2026-03-10 07:14:00 +0100

    ajout de la page de global gam jam

--- src/applications/app-locales/opera/app/pages/page/page_global_game_jam_2026.js
+++ src/applications/app-locales/opera/app/pages/page/page_global_game_jam_2026.js
@@ -2,8 +2,8 @@ import {game_jam} from "./tags/liste_de_tag.js";
 
 let contenue = document.createElement("div");
 contenue.innerHTML =
-    "<iframe src='ressources/documents/work_in_progress.html'>" +
+    "<iframe src='ressources/documents/global_game_jam.html'>" +
     "</iframe>"
 
 
-export let page_global_game_jam_2026 = ["global game jam 2026", contenue, game_jam]
+export let page_global_game_jam_2026 = ["Automate Mascarade", contenue, game_jam.concat('Game Design', 'Studio MERCIER Béziers')]
